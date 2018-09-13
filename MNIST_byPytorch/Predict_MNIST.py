7b9369a811b7f8028c837bd6b7faaac410d2a3a0
shota_okuno <shota00018@icloud.com>
2018-09-13 10:08:10 +0900

    made Get_MNIST.py

--- MNIST_byPytorch/Predict_MNIST.py
+++ MNIST_byPytorch/Predict_MNIST.py
@@ -1,9 +1,29 @@
-# ハイパーパラメータの設定
+#モジュールのインポート
 
-in_dim  = 784
-hid_dim = 200
-out_dim = 10
-lr = 0.001
-batch_size = 32
-num_epochs = 40
+import sys, os
+sys.path.append('../')
+from Pytorch_MLP import MLP
+import Get_MNIST
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import csv
+import torch.optim as optim
+import torch.autograd as autograd
+import torch.nn.functional as F
+import torchvision
+from torchvision import datasets, transforms, models
+from sklearn.metrics import f1_score
+
+#モデリング
+#MLPで実装
+
+mlp = MLP(in_dim, hid_dim, out_dim)
+
+optimizer = optim.SGD(mlp.parameters(),
+    lr = lr)
+
+criterion = nn.NLLLoss()
+
 
