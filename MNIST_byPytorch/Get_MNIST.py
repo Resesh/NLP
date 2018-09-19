8378ffedf59974c8bc514363d45e31aae29194ad
shota_okuno <shota00018@icloud.com>
2018-09-19 23:30:00 +0900

    modified

--- MNIST_byPytorch/Get_MNIST.py
+++ MNIST_byPytorch/Get_MNIST.py
@@ -1,48 +1,21 @@
 #pytorchのモジュールを利用する場合
 
-import csv
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.autograd as autograd
-import torch.nn.functional as F
 import torchvision
 from torchvision import datasets, transforms, models
-from sklearn.metrics import f1_score
 
-
-# ハイパーパラメータの設定
-
-in_dim  = 784
-hid_dim = 200
-out_dim = 10
-lr = 0.001
-batch_size = 32
-num_epochs = 40
-
-
-
-train_data = torchvision.datasets.FashionMNIST(
+def get_mnist(train_data, test_data):
+    
+    train_data = torchvision.datasets.FashionMNIST(
     './data/fashion-mnist', train = True,
     transform = torchvision.transforms.ToTensor(), download = True
     )
 
-test_data = torchvision.datasets.FashionMNIST(
+    test_data = torchvision.datasets.FashionMNIST(
     './data/fashion-mnist', train = True, 
     transform = torchvision.transforms.ToTensor(), download = True
 )
+    return train_data, test_data
 
-#define DataLoader
 
-train_data_loader = torch.utils.data.DataLoader(
-    dataset = train_data,
-    batch_size = batch_size,
-    shuffle = True
-)
-
-test_data_loader = torch.utils.data.DataLoader(
-    dataset = test_data,
-    batch_size = batch_size,
-    shuffle = True
-)
--- MNIST_byPytorch/Predict_MNIST.py
+++ MNIST_byPytorch/Predict_MNIST.py
@@ -1,9 +1,6 @@
 #モジュールのインポート
 
-import sys, os
-sys.path.append('../')
-from Pytorch_MLP import MLP
-import Get_MNIST
+import MLP_Pytorch
 import numpy as np
 import torch
 import torch.nn as nn
@@ -15,15 +12,67 @@ import torch.nn.functional as F
 import torchvision
 from torchvision import datasets, transforms, models
 from sklearn.metrics import f1_score
+from Get_MNIST import get_mnist
+
+
+# ハイパーパラメータの設定
+
+in_dim  = 784
+hid_dim = 200
+out_dim = 10
+lr = 0.001
+batch_size = 32
+num_epochs = 40
+
+#define DataLoader
+#train, test = get_mnist(train_data, test_data)
+
+train_data = torchvision.datasets.FashionMNIST(
+    './data/fashion-mnist',
+    transform=torchvision.transforms.ToTensor(),
+    train=True,
+    download=True)
+
+test_data = torchvision.datasets.FashionMNIST(
+    './data/fashion-mnist',
+    transform=torchvision.transforms.ToTensor(),
+    train=False,
+    download=True)
+
+train_data_loader = torch.utils.data.DataLoader(
+    dataset = train_data,
+    batch_size = batch_size,
+    shuffle = True
+)
+
+test_data_loader = torch.utils.data.DataLoader(
+    dataset = test_data,
+    batch_size = batch_size,
+    shuffle = True
+)
+
+
 
 #モデリング
 #MLPで実装
 
-mlp = MLP(in_dim, hid_dim, out_dim)
+mlp = MLP_Pytorch.MLP(in_dim, hid_dim, out_dim)
 
-optimizer = optim.SGD(mlp.parameters(),
-    lr = lr)
-
+optimizer = optim.SGD(mlp.parameters(), lr = lr)
 criterion = nn.NLLLoss()
+
+for epoch in range(num_epochs + 1):
+    losses_train = []
+    losses_test = []
+    preds_train = []
+    preds_test = []
+    trues_train = []
+    trues_test = []
+
+    mlp.train()
+
+    for x, t in train_data_loader:
+        true = t.tolist()
+        
 
 
