c0171436173e9dc22998d5997627296f1a70c0c2
Shota Okuno <shota@ShotanoMacBook-Pro.local>
2018-10-01 19:09:49 +0900

    code CBOW

--- Word2Vec/CBOW.py
+++ Word2Vec/CBOW.py
@@ -1,1 +1,15 @@
 #implement CBOW(Continuous Bug Of Words)
+
+#setting hypermparameters
+
+
+batch_size = 64
+n_batches = 500
+vocab_size = len(vocab.word2id)
+embedding_size = 300
+
+"batch_size: ミニバッチのサイズ"
+"n_batches: 今回学習するミニバッチの数"
+"vocab_size: 語彙の総数"
+"embedding_size: 各単語の次元数"
+
