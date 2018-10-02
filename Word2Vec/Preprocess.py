8e88c911a17e0acaa53187dd821bd2a2219056f1
Shota Okuno <shota@ShotanoMacBook-Pro.local>
2018-10-03 00:58:55 +0900

    where Mecab

--- Word2Vec/Preprocess.py
+++ Word2Vec/Preprocess.py
@@ -1,1 +1,17 @@
 #define functions implemented in preprocessing
+
+import Mecab
+import numpy as np
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+tagger = Mecab.tagger("-Ochasen")
+node = tagger.parse("坊主が屏風に上手に坊主の絵を描いた")
+print(node)
+
